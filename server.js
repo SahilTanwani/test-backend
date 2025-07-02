c0b96bfdd8e62837ea6cf84c83a5757e023173c3
Sahil Tanwani <sahiltanwani24@gmail.com>
2025-07-03 03:38:39 +0530

    Enabled CORS, Google login endpoint ready

--- server.js
+++ server.js
@@ -9,7 +9,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(cors({
+  origin: "https://test-cofx.vercel.app",  // Your deployed frontend URL
+  credentials: true
+}));
 app.use(express.json());
 
 app.use('/api/users', userRoutes);
